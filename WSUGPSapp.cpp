9027601560a1e40139d3bb90a9c2c3203de037cd
Michael Hull <Michaelhullzero@gmail.com>
2026-05-02 22:29:26 -0700

    setup SFML

--- WSUGPSapp.cpp
+++ WSUGPSapp.cpp
@@ -1,20 +1,19 @@
-// WSUGPSapp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <SFML/Graphics.hpp>
 
-#include <iostream>
+int main() {
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "WSU Graph Project");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-int main()
-{
-    std::cout << "Hello World!\n";
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
